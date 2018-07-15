434c2ad3a62e4421b96ca14cffa88995fc602c99
Kristen Papke <klpapke215@gmail.com>
2018-07-15 19:51:05 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -11,8 +11,8 @@ def retrieval
   shipping_manifest = {
                   "whale bone corsets" => 5,
                   "porcelain vases" => 2,
-                  "oil paintings" => 3
-  shipping_manifest["oil paintings"]               }
+                  "oil paintings" => 3}
+  shipping_manifest["oil paintings"]             
 end
 
 
